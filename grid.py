cfb95968886bc7c1523ea235f93d38036f6086ab
Timothée jouffroy <timothee.jouffroy@etu.minesparis.psl.eu>
2026-01-30 11:05:05 +0100

    commit 1 grid

---
+++ grid.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pyxel
+import random as rd
+
+
+GRID_SIZE=30
+INITIAL_SHEEP=50
+INITIAL_WOLF=10
+INITIAL_GRASS_COVERAGE=30
+
+class grid:
+    def __init__(self):
+        
+        self.is_wolf=[]
+        self.is_sheep=[]
+        self.is_grass=[]
+
+        while len(self.is_grass) < np.floor(INITIAL_GRASS_COVERAGE*GRID_SIZE**2/100):
+            Tile=(rd.randint(0,GRID_SIZE),rd.randint(0,GRID_SIZE))
+            if grass(Tile) not in self.is_grass:
+                self.is_grass += grass(Tile)
+
+        while len(self.is_sheep) < INITIAL_SHEEP:
+            Tile=(rd.randint(0,GRID_SIZE),rd.randint(0,GRID_SIZE))
+            if sheep(Tile) not in self.is_sheep:
+                self.is_grass += sheep(Tile)
+
+        while len(self.is_wolf) < INITIAL_WOLF:
+            Tile=(rd.randint(0,GRID_SIZE),rd.randint(0,GRID_SIZE))
+            if wolf(Tile) not in self.is_wolf:
+                self.is_wolf += wolf(Tile)
+        
+
+    def draw(self):
+        pyxel.init(GRID_SIZE, GRID_SIZE, title="Simulation")
+        pyxel.cls(15)
+
+    def start(self):
+        pyxel.run(self.draw)
+
+
+grid().start()
+
+        
+
