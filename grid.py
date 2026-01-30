00e45a572531eb9ada41438cf56fff505b6e11d8
Timothée jouffroy <timothee.jouffroy@etu.minesparis.psl.eu>
2026-01-30 13:44:13 +0100

    grid with sheep + wolf

--- grid.py
+++ grid.py
@@ -4,53 +4,58 @@ import random as rd
 
 
 GRID_SIZE=30
+TILE=16
 INITIAL_SHEEP=50
 INITIAL_WOLF=10
 INITIAL_GRASS_COVERAGE=30
 
 class wolf:
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
+    def draw(self):
+        pyxel.blt(self.x*TILE, self.y*TILE, 1, 0, 0, 16, 16, 0)
 
 class sheep:
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
+    def draw(self):
+        pyxel.blt(self.x*TILE, self.y*TILE, 0, 0, 0, 16, 16, 0)
 
 class grass:
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
 class grid:
     def __init__(self):
-        self.damier = []
-        for i in range(GRID_SIZE):
-            for j in range(GRID_SIZE):
-                if (i + j) % 2 == 0:
-                    self.damier.append((i, j))
 
         self.is_wolf=[]
         self.is_sheep=[]
         self.is_grass=[]
 
+        def libre(x, y):
+            return all(obj.x != x or obj.y != y
+                       for obj in (self.is_sheep + self.is_wolf))
+
         while len(self.is_grass) < np.floor(INITIAL_GRASS_COVERAGE*GRID_SIZE**2/100):
-            x1, y1 =rd.randint(0,GRID_SIZE), rd.randint(0,GRID_SIZE)
+            x1, y1 =rd.randint(0,GRID_SIZE-1), rd.randint(0,GRID_SIZE-1)
             if grass(x1,y1) not in self.is_grass:
                 self.is_grass += [grass(x1, y1)]
 
         while len(self.is_sheep) < INITIAL_SHEEP:
-            x1, y1=rd.randint(0,GRID_SIZE), rd.randint(0,GRID_SIZE)
-            if sheep(x1, y1) not in self.is_sheep:
+            x1, y1=rd.randint(0,GRID_SIZE-1), rd.randint(0,GRID_SIZE-1)
+            if libre(x1, y1):
                 self.is_sheep += [sheep(x1, y1)]
 
         while len(self.is_wolf) < INITIAL_WOLF:
-            x1, y1=rd.randint(0,GRID_SIZE), rd.randint(0,GRID_SIZE)
-            if wolf(x1, y1) not in self.is_wolf and sheep(x1,y1) not in self.is_sheep:
+            x1, y1=rd.randint(0,GRID_SIZE-1), rd.randint(0,GRID_SIZE-1)
+            if libre(x1, y1):
                 self.is_wolf += [wolf(x1, y1)]
 
-        pyxel.init(GRID_SIZE, GRID_SIZE, title="Simulation")
+        pyxel.init(GRID_SIZE*TILE, GRID_SIZE*TILE, title="Simulation")
+        pyxel.load("sim.pyxres")
         
         
         def remove_wolf(self, pos): # pos est un couple (x,y)
@@ -65,12 +70,13 @@ class grid:
         
 
     def draw(self):
-        pyxel.cls(15)
-        for x, y in self.damier:
-            pyxel.rect(x, y, 1, 1, 4)
-        for tile in self.is_grass:
-            pyxel.rect(tile.x,tile.y,1,1,3)
-        
+        pyxel.cls(4)
+        for g in self.is_grass:
+            pyxel.rect(g.x*TILE, g.y*TILE, TILE, TILE, 3)
+        for s in self.is_sheep:
+            s.draw()
+        for w in self.is_wolf:
+            w.draw()
 
     def update(self):
         pass
