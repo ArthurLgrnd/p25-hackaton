a553b4c66496c993b5e3f1854a7d8fb23726a7d7
Quentin Borde <quentin.borde@etu.minesparis.psl.eu>
2026-01-30 11:37:09 +0100

    Entities V1

--- entities.py
+++ entities.py
@@ -3,16 +3,17 @@ import random as rd
 
 
 
-class sheep :
+class sheep() :
     
-    def __init__(self, x,y,energy,age,is_grass): # is_grass: pos des herbes adjacentes
+    def __init__(self, x,y,energy,age): 
         self.x=x
         self.y=y
         self.energy=energy
         self.age=age
         self.mort=False
         self.repro=False
-    
+
+    """  # Pour savoir si on est sur le bord
     def cases_dispo(self, grid_size):
         if self.x==0:
             if self.y==0:
@@ -43,15 +44,10 @@ class sheep :
         else:
                 return([(0,1),(0,-1),(1,0),(-1,0)])                        
 
-
-    def Deplacement(self,grid_size): # ATTENTION potentielle redondance avec Simulation, format liste (xmax,ymax)          
-        
-        
-        cases_possibles=cases_dispo(self,grid_size)
-        (dx,dy)=rd.choice(cases_possibles)
-        self.x+=dx
-        self.y+=dy
-    
+"""
+    def Deplacement(self,cases): # cases : liste des cases possibles (tri si grass déjà fait)     
+        (self.x,self.y) += rd.choice(cases)
+ 
     def Alimentation(self): # Lancé par sim 
         self.energy+=10
     
@@ -60,31 +56,49 @@ class sheep :
               self.energy-=20
               self.repro=True 
 # ATTENTION: il faut que Simulation créée un nv mouton avec 20 d'energie et mette repro à False
+    
     def meurt(self,age_limite):  # Âge limite à définir dans simulation
          if self.energy<=0 or self.age > age_limite:
               self.mort=True
               
 
-
-        
-class wolf :
+class wolf() :
     
-    def __init__(self,position,energy,age,):
+    def __init__(self,x,y,energy,age): 
         self.x=x
         self.y=y
         self.energy=energy
         self.age=age
         self.mort=False
-        self.repro=False
+        self.repro=False              
 
+    def Deplacement(self,cases): # cases : liste des cases possibles (tri si sheep déjà fait)     
+        (self.x,self.y) += rd.choice(cases)
 
+    def Alimentation(self): # Lancé par sim 
+        self.energy+=30 
 
+    def Reproduction(self,seuil):  #Seuil à définir dans main
+         if self.energy > seuil:
+              self.energy-=20
+              self.repro=True 
+# ATTENTION: il faut que Simulation créée un nv loup avec 40 d'energie et mette repro à False
 
+    def meurt(self,age_limite):  # Âge limite à définir dans simulation
+         if self.energy<=0 or self.age > age_limite:
+              self.mort=True   
 
-class grass :
+class grass() :
     
-    def __init__(self,
-                 state,
-                 regrowth_rate,      
-    ):
+    def __init__(self,x,y,state): # eventuellement retirer x et y
+         self.x=x
+         self.y=y
+         self.state=state # Bool
+         self.compt=0
+
+    def growing(self):
+         self.compt+=1
+         if self.compt==7: # Eventuellement l'appeler regrowth_rate et le mettre en cte sur main
+              self.state=True
+              self.compt=0
         
